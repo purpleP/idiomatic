b1d5b1e84346ad704654e1da3a8214e294ff0322
mikhail.doronin <mikhail.doronin@alibaba-inc.com>
2019-12-18 10:49:27 +0300

    Check extension by using glob

--- Idiomatic.java
+++ Idiomatic.java
@@ -1,9 +1,18 @@
 import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 class Idiomatic {
 
     String[] pathsFromClassPath() {
         return System.getProperty("java.class.path").split(File.pathSeparator);
+    }
+
+    boolean checkFileExtension(Path p, String extension) {
+        return FileSystems
+            .getDefault()
+            .getPathMatcher("glob:*." + extension)
+            .matches(p);
     }
 
     private static void main(String... args) {
